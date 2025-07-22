ffda54235c747a53d459ac3c93a4305d29f6043c
Bhavishya P T <ptbhavishya@gmail.com>
2025-07-22 13:14:58 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,32 @@
+async function getWeather() {
+  const city = document.getElementById("city").value;
+  const result = document.getElementById("weatherResult");
+
+  const apiKey = "e475418c0c1035cfb364a921eb836e3c";
+
+  if (city === "") {
+    result.innerHTML = "❗ Please enter a city name.";
+    return;
+  }
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric" ;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.cod === 200) {
+      result.innerHTML = `
+        <h2>${data.name}, ${data.sys.country}</h2>
+        <p><strong>🌡 Temp:</strong> ${data.main.temp} °C</p>
+        <p><strong>☁ Weather:</strong> ${data.weather[0].main}</p>
+        <p><strong>💧 Humidity:</strong> ${data.main.humidity}%</p>
+        <p><strong>🌬 Wind:</strong> ${data.wind.speed} m/s</p>
+      `;
+    } else {
+      result.innerHTML = "❗ City not found. Please try again.";
+    }
+  } catch (error) {
+    result.innerHTML = "⚠ Error fetching data. Try again later.";
+  }
+}
